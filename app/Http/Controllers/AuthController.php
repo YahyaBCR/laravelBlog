8550af18f4ef880204e490cd070c8be57eb5151d
Bouachra Yahya <yahyasaw01@gmail.com>
2026-03-06 15:56:26 +0000

    Implement user registration with Sanctum authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request){
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'user created',
+            'token' => $token,
+            'user' => $user,
+        ], 201);
+    }
+}
